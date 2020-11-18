f60bf927ae684bbf303d35219216f18ce41ed66f
purushothaman-source <purushoth99jun@gmail.com>
2020-11-18 12:47:53 +0530

    ADDED:Email Pattern

--- userregistration.sh
+++ userregistration.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 shopt -s extglob
-lastNamePat="^[A-Z]{1}[a-z]{2,}$"
-read -p "enter lastname " lastName
+emailPat="^[0-9a-zA-Z]+([.\-_+][0-9a-zA-Z]+)*@[a-z0-9A-Z]+.[a-z]{2,4}([.][a-zA-Z]{2})*$"
+read -p "enter valid email " email
 
-if [[ $lastName =~ $lastNamePat ]]
+if [[ $email =~ $emailPat ]]
 then
    echo "Pattern matching"
 else
